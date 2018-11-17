7818f1f17b25023c42943ad4fd2d32f1060cffce
Eugene Otto <eugene.otto@gmail.com>
2018-11-17 02:16:53 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+
+## Installing
+
+```bash
+yarn install
+```
+
+## How to start
+
+```bash
+yarn start
+```
+
+
---
+++ server.js
@@ -0,0 +1,35 @@
+const Koa = require('koa')
+const Router = require('koa-router')
+const axios = require('axios')
+
+const app = new Koa()
+const router = new Router()
+
+router.get('/', async (ctx, next) => {
+  // Fetch CryptoKitty
+  const kittyId = 116
+  const url = `https://public.api.cryptokitties.co/v1/kitties/${kittyId}`
+  const kittyJson = await axios.get(url, {
+    headers: {
+      'x-api-token': 'YOUR-CRYPTKITTY-API-KEY'
+    }
+  })
+
+  // Respond with CryptoKitty data
+  ctx.body = kittyJson.data
+})
+
+router.post('/move', (ctx, next) => {
+  // Do something...
+
+  // Set response
+  ctx.body = {
+    status: 'hello poster'
+  }
+})
+
+const port = 4000
+app
+  .use(router.routes())
+  .use(router.allowedMethods())
+  .listen(port)
